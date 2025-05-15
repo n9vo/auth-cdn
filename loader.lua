462be7ecaa37d91775d412df6373340374676f55
bannisters <73430720+n9vo@users.noreply.github.com>
2025-05-15 01:44:53 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,46 @@
-local r=(syn and syn.request)or(SENTINEL_V2 and function(o)return{StatusCode=200,Body=request(o.Url,o.Method,o.Body or"")}end)or request or function()return{Body="{}"}end
-local ok,err=pcall(assert(loadstring((function(u)local ok,res=pcall(r,{Url=u,Method="GET",Headers={["Content-Type"]="application/json"}})return ok and res.Body or error"[9auth] Failed to download auth helper script."end)(("https://9auth.xyz/loader/%s/%s"):format(_G.AUTH_SERVER_ID,_G.AUTH_SCRIPT_ID))),"[9auth] Error compiling payload."));if not ok then warn("[9auth] Error running payload:",err)end
+local StarterGui = game:GetService("StarterGui")
+
+local function notify(msg)
+    StarterGui:SetCore("SendNotification", {
+        Title = "9AUTH",
+        Text = msg,
+        Duration = 5
+    })
+end
+
+local httpRequest =
+    (syn and syn.request) or
+    (SENTINEL_V2 and function(opt)
+        return {
+            StatusCode = 200,
+            Body = request(opt.Url, opt.Method, opt.Body or "")
+        }
+    end) or
+    request or
+    function()
+        return { Body = "{}" }
+    end
+
+local url = ("https://9auth.xyz/loader/%s/%s"):format(_G.AUTH_SERVER_ID, _G.AUTH_SCRIPT_ID)
+
+local ok, err = pcall(function()
+    local success, res = pcall(httpRequest, {
+        Url = url,
+        Method = "GET",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        }
+    })
+
+    if not success then
+        error("[9auth] Failed to download loader")
+        notify("Failed to download loader")
+    end
+
+    assert(loadstring(res.Body))()
+end)
+
+if not ok then
+    warn("[9auth] Failed to run loader")
+    notify("Failed to run loader")
+end
